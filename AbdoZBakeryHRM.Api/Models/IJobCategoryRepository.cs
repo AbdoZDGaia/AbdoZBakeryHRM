b6659f314c259afa254e9e54e50f29693c7658f4
AbdoZDGaia <oldtimeprogrammer@yahoo.com>
2021-02-01 18:22:49 +0200

    API integrated

---
+++ AbdoZBakeryHRM.Api/Models/IJobCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using AbdoZBakeryHRM.Shared;
+
+namespace AbdoZBakeryHRM.Api.Models
+{
+    public interface IJobCategoryRepository
+    {
+        IEnumerable<JobCategory> GetAllJobCategories();
+        JobCategory GetJobCategoryById(int jobCategoryId);
+    }
+}
